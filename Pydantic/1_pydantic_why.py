68dbd1a37450f4dddac454abe62d56310217d91c
Talha Imran <mtalhaimran104@gmail.com>
2026-09-10 01:45:41 +0500

    feat: learn some data types in pydantic

--- Pydantic/1_pydantic_why.py
+++ Pydantic/1_pydantic_why.py
@@ -1,12 +1,14 @@
-from pydantic import BaseModel
-from typing import List , Dict
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List , Dict , Optional
 
 class Patient(BaseModel):
-    name: str
-    age: int
+    name: str = Field(max_length= 50)
+    email: EmailStr
+    linkedin: AnyUrl
+    age: int = Field(gt = 10, lt = 50)
     weight: float
     married: bool
-    allergies: list[str]
+    allergies: Optional[list[str]] = None
     contact: Dict[str, str]
 
 def insert_patient_data(patient: Patient):
@@ -17,12 +19,19 @@ def insert_patient_data(patient: Patient):
 
 def update_patient_data(patient: Patient):
     print(patient.name)
+    print(patient.email)
+    print(patient.linkedin)
     print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact)
     print('updated')
 
 
-patient_info = {'name': 'talha', 'age': '20', 'weight': 67.5, 'married': True, 'allergies': ['throat', 'dust'] , 'contact': {'gmail': 'talha@gamil.com' , 'phone': 123456}}
+patient_info = {'name': 'talha', 'email': 'mtalhaimran104@gmail.com', 'linkedin': 'https://www.linkedin.com/in/talha-imran-ai/', 'age': '20', 'weight': 67.5, 'married': True, 'contact': { 'phone': '1233', 'landline': '124321'}}
 
 patient1 = Patient(**patient_info)
 
 update_patient_data(patient1)
+
